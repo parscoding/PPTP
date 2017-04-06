84d6bb5ed54714177d5e37b7babefa93c74cb240
Majid <majid.a.iran@gmail.com>
2017-04-06 17:50:36 +0430

    Update installer.sh
    first step

--- installer.sh
+++ installer.sh
@@ -1,1 +1,52 @@
-#bash
+# Created by Majid Abdollahi.
+# Available on https://parscoding.github.io/pptp
+
+#!/bin/bash -e
+
+# change iptables settings for this operation
+# ---------------------------------------------------------
+# iptables example configuration script
+#
+# Flush all current rules from iptables
+#
+ iptables -F
+#
+# Allow SSH connections on tcp port 22
+# This is essential when working on remote servers via SSH to prevent locking yourself out of the system
+#
+ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+ 
+  #for pptpd
+#----------------------------------------------------------
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A INPUT -i eth0 -p tcp --dport 1723 -j ACCEPT
+iptables -A INPUT -i eth0 -p gre -j ACCEPT
+#----------------------------------------------------------
+
+#
+# Set default policies for INPUT, FORWARD and OUTPUT chains
+#
+ iptables -P INPUT DROP
+ iptables -P FORWARD ACCEPT
+ iptables -P OUTPUT ACCEPT
+#
+# Set access for localhost
+#
+ iptables -A INPUT -i lo -j ACCEPT
+#
+# Accept packets belonging to established and related connections
+#
+ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+#
+# Save settings
+#
+service iptables save
+#
+# List rules
+#
+# Restart iptables Service
+service iptables restart
+
+# make iptables service as startup
+chkconfig iptables on
+
